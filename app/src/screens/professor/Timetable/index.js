dc4955f2b9a6fde8fb3828697f318cd8f09c806e
Geison Machado <geisonmcd@gmail.com>
2021-02-28 19:21:18 -0300

    coisas

--- app/src/screens/professor/Timetable/index.js
+++ app/src/screens/professor/Timetable/index.js
@@ -9,15 +9,21 @@ import { useNavigation, useTheme, useRoute } from '@react-navigation/native';
 import { useAuthContext } from '../../../contexts/AuthContext';
 import { useAppContext } from '../../../contexts/AppContext';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import DatePicker from 'react-native-date-picker'
+import Moment from 'moment';
+
 MaterialIcons.loadFont();
 
 export default function Timetable({ navigation }) {
+
     const { } = useAuthContext();
     const { } = useAppContext();
     const route = useRoute();
-
+    const [startModalVisible, setStartModalVisible] = useState(false);
+    const [endModalVisible, setEndModalVisible] = useState(false);
     const { timetable } = route.params;
-
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
 
     useEffect(() => {
     }, []);
@@ -27,17 +33,72 @@ export default function Timetable({ navigation }) {
             <Page
                 type="static"
                 title={translate('Timetable')}
+                subtitle={timetable.name}
                 smallTitle={translate('Diaries')}
             >
-                <View style={{padding: 10}}>
-                    <Text>{timetable.name}</Text>
+                <View style={{ padding: 10 }}>
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={startModalVisible}
+                    >
+                        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                            <View style={[styles.modalView, { elevation: 5, backgroundColor: '#eee', }]}>
+                                <DatePicker
+                                    date={startDate}
+                                    onDateChange={setStartDate}
+                                    mode="time"
+                                />
+                                <Button style={styles.button} title={'OK'} onPress={() => setStartModalVisible(false)} />
+                            </View>
+                        </View>
+                    </Modal>
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={endModalVisible}
+                    >
+                        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                            <View style={[styles.modalView, { elevation: 5, backgroundColor: '#eee', }]}>
+                                <DatePicker
+                                    date={startDate}
+                                    onDateChange={setEndDate}
+                                    mode="time"
+                                />
+                                <Button style={styles.button} title={'OK'} onPress={() => setEndModalVisible(false)} />
+                            </View>
+                        </View>
+                    </Modal>
+                    <View style={{ justifyContent: 'center' }}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                            <Text style={{ color: 'black', paddingLeft: 5, fontSize: 16 }} >Start Date</Text>
+                            <Text style={{ color: 'black', paddingLeft: 5, fontSize: 16 }} >End Date</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                            <Pressable onPress={() => setStartModalVisible(true)}>
+                                <Text style={styles.textInput} placeholderTextColor="#fff" >{Moment(startDate).format('hh:mm')} </Text>
+                            </Pressable>
+                            <Pressable onPress={() => setEndModalVisible(true)}>   
+                                <Text style={styles.textInput} placeholderTextColor="#fff" >{Moment(endDate).format('hh:mm')} </Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                    <Button style={styles.button} title={'ADD'} onPress={() => setModalVisible(false)} />
                 </View>
             </Page>
         </View>
     );
 }
 const styles = StyleSheet.create({
-   
+    textInput: {
+        borderWidth: 0,
+        borderRadius: 4,
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+        fontSize: 16,
+        marginBottom: 4,
+        backgroundColor: '#575e67',
+        color: '#fff',
+        textAlign: 'center'
+    },
 });
-
-
